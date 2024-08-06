314eacf059b762a5b8f270719a0ec998ada9e815
HP <sindhavhardik001@gmail.com>
2024-08-06 23:48:00 +0530

    set local database

---
+++ lib/service/shared_preferences_service.dart
@@ -0,0 +1,38 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferencesService {
+  SharedPreferences? _preferences;
+
+  String countryKey = "country_key";
+  String stateKey = "state_key";
+  String cityKey = "city_key";
+
+  static final SharedPreferencesService _instance =
+      SharedPreferencesService._internal();
+
+  factory SharedPreferencesService() {
+    return _instance;
+  }
+
+  SharedPreferencesService._internal();
+
+  Future<void> init() async {
+    _preferences = await SharedPreferences.getInstance();
+  }
+
+  Future<void> setString(String key, String value) async {
+    await _preferences?.setString(key, value);
+  }
+
+  String? getString(String key) {
+    return _preferences?.getString(key);
+  }
+
+  Future<void> remove(String key) async {
+    await _preferences?.remove(key);
+  }
+
+  Future<void> clear() async {
+    await _preferences?.clear();
+  }
+}
